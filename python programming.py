d44ec6668302846f69fc5cc375174dbf1f227f00
Binisha Adhikari <binishaadhikari24@gmail.com>
2024-04-19 15:55:07 +0545

    New branch code is added

---
+++ python programming.py
@@ -0,0 +1,37 @@
+python programming
+java programming 
+
+#pascalcase
+Pythonprogramming = "python"
+Javaprogramming = "java"
+print (id(pythonprogramming))
+print(id(javaprogramming))
+
+
+#snakecase
+python_programming = "python"
+java_programming ="java"
+print(id(python_programming))
+ptint(id(java_programming))
+
+#flatcase
+pythonprogramming = "python"
+javaprogramming = "java"
+print(id(pythonprogramminng))
+print(id(javaprogramming))
+
+#upperflat case
+PYTHONPROGRAMMING = "python"
+JAVAPROGRAMMING = "java"
+print(id(PYTHONPROGRAMMING))
+PRINT(ID(JAVAPROGRAMMING))
+
+#camelsnakecase
+python_Programming = "python"
+java_Programming = "java"
+print(id(python_Programming))
+print(id(java_programming))
+
+#pascalsnakecase
+Python_Programming = "python"
+Java_PRO
